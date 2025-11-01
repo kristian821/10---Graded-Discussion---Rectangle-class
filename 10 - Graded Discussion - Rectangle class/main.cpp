dd530f8e62a274edcc6b7f0dfb2b458b93907327
Kristian Mitchell <kmitch79@mail.ccsf.edu>
2025-11-01 15:48:13 -0700

    Initial Commit

---
+++ 10 - Graded Discussion - Rectangle class/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  10 - Graded Discussion - Rectangle class
+//
+//  Created by Kristian Mitchell on 11/1/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
